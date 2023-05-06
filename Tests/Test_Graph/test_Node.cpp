efbad6a1cbce427bea9bf486f077ad49f8bf16c4
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-05-06 16:59:13 +0300

    short test for node

--- Tests/Test_Graph/test_Node.cpp
+++ Tests/Test_Graph/test_Node.cpp
@@ -1,10 +1,22 @@
 #include <catch2/catch_all.hpp>
+#include "Graph/Graph.h"
 
 TEST_CASE( "Node", "[Node]")
 {
 
-    SECTION("Creation")
+    SECTION("Iterators")
     {
+        Graph<int> graph;
+        auto itParent = graph.addNode(0);
+
+        for (int i = 1; i <= 50; ++i)
+            graph.addNode(i, {{0,&(itParent->second)}});
+
+        int count = 0;
+        for (auto it = itParent->second.beginNeighbours(); it != itParent->second.endNeighbours(); it++)
+            count++;
+
+        CHECK(count == 50);
 
     }
 
