beac7284c9b795b2afbe22987853d7475a315a80
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2023-05-09 19:19:20 +0300

    added basic user info logger

---
+++ Libs/Logging/UserInfo.cpp
@@ -0,0 +1,34 @@
+#include "UserInfo.h"
+
+UserInfo::UserInfo(std::string username) : m_username(std::move(username))
+{
+
+}
+
+void UserInfo::initStartingInfo(const std::string &startingWord, const std::string &finalWord, int optimalNoMoves,
+                                const AppClock_t &start)
+{
+    if (startingWord.size() != finalWord.size())
+        throw std::logic_error("Word size does not match");
+
+    m_startingWord = startingWord;
+    m_finalWord = finalWord;
+    m_optimalNoMoves = optimalNoMoves;
+
+    m_startingTime = start;
+}
+
+void UserInfo::usedHint(int noUsedHints)
+{
+    m_hintsUsed += noUsedHints;
+}
+
+void UserInfo::madeMove(int noMadeMoves)
+{
+    m_movesMade += noMadeMoves;
+}
+
+void UserInfo::finishTime(const AppClock_t &finish)
+{
+    m_finishTime = finish;
+}
---
+++ Libs/Logging/UserInfo.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <chrono>
+
+using AppClock_t = std::chrono::time_point<std::chrono::system_clock>;
+class UserInfo
+{
+private:
+    std::string m_username;
+
+    int m_hintsUsed = 0;
+    std::string m_startingWord;
+    std::string m_finalWord;
+    std::vector<std::string> m_wordsUsed;
+    int m_movesMade = 0;
+    int m_optimalNoMoves;
+
+    AppClock_t m_startingTime;
+    AppClock_t m_finishTime;
+
+public:
+    explicit UserInfo(std::string username);
+
+    void initStartingInfo(const std::string &startingWord, const std::string &finalWord,
+                          int optimalNoMoves, const AppClock_t &start);
+
+    void usedHint(int noUsedHints = 1);
+    void madeMove(int noMadeMoves = 1);
+
+    void finishTime(const AppClock_t &finish);
+};
